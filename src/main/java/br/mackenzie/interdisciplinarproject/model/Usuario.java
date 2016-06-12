78693450be87dcf58edf061937adcbd322ddfe1b
Jony Fernando Schulz Air <jonymackenzie@gmail.com>
2016-06-12 16:03:23 -0300

    Commit referente a listagem de monitores com foto de perfil ja sendo trazida no front

--- src/main/java/br/mackenzie/interdisciplinarproject/model/Usuario.java
+++ src/main/java/br/mackenzie/interdisciplinarproject/model/Usuario.java
@@ -25,25 +25,32 @@ public class Usuario {
 	private String nome;
 	@Column(name = "email")
 	private String email;
+	@Column(name = "img_perfil")
+	private String img_perfil;
+	@Column(name = "tipo_monitor")
+	private String tipo_monitor;
 	
 	/**
 	 * Construtor com seus parâmetros.
 	 * @param tia
 	 * @param nome
 	 * @param email
+	 * @param img_perfil
 	 */
-	public Usuario(int tia, String nome, String email) {
+	public Usuario(int tia, String nome, String email, String img_perfil, String tipo_monitor) {
 		super();
 		this.tia = tia;
 		this.nome = nome;
 		this.email = email;
+		this.img_perfil = img_perfil;
+		this.tipo_monitor = tipo_monitor;
 	}
 	/**
 	 * Construtor vazio.
 	 */
 	public Usuario() {
 		super();
-	}
+	}	
 
 	/**
 	 * Métodos que retornam seus respectivos dados.
@@ -66,5 +73,17 @@ public class Usuario {
 	}
 	public void setEmail(String email) {
 		this.email = email;
-	}	
+	}
+	public String getImg_perfil() {
+		return img_perfil;
+	}
+	public void setImg_perfil(String img_perfil) {
+		this.img_perfil = img_perfil;
+	}
+	public String getTipo_monitor() {
+		return tipo_monitor;
+	}
+	public void setTipo_monitor(String tipo_monitor) {
+		this.tipo_monitor = tipo_monitor;
+	}
 }
